31391fd10d0a7ed71fe2458e2ded1ea549b69194
Garon <garoncdavis@gmail.com>
2017-01-31 14:27:07 -0500

    created clock module and styled it

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,6 +1,12 @@
 var React = require('react');
 
 var Clock = React.createClass({
+	getDefaultProps() {
+		totalSeconds: 0
+	}, 
+	PropTypes: {
+		totalSeconds: React.PropTypes.number
+	},
 	formatSeconds : function (totalSeconds) {
 		var seconds = totalSeconds % 60;
 		var minutes = Math.floor(totalSeconds / 60);
@@ -12,9 +18,13 @@ var Clock = React.createClass({
 		return minutes + ':' + seconds;
 	},
 	render: function() {
-		return (
-			<div> 
+		var {totalSeconds} = this.props;
 
+		return (
+			<div className="clock"> 
+				<span className="clock-text"> 
+					{this.formatSeconds(totalSeconds)}
+				</span>
 			</div>
 		);
 	}
