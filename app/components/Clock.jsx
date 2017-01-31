ece1163b0b65f6df0cfaff8b0eb195c42451b6ec
Garon <garoncdavis@gmail.com>
2017-01-31 14:11:01 -0500

    added test to test against function when less than 10 seconds are returned

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -5,6 +5,10 @@ var Clock = React.createClass({
 		var seconds = totalSeconds % 60;
 		var minutes = Math.floor(totalSeconds / 60);
 
+		seconds < 10 ? seconds = '0' + seconds : seconds
+
+		minutes < 10 ? minutes = '0' + minutes : minutes
+
 		return minutes + ':' + seconds;
 	},
 	render: function() {
--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -14,8 +14,17 @@ describe('Clock', () => {
 	describe('formatSeconds', () => {
 		it('should format seconds', () => {
 			var clock = TestUtils.renderIntoDocument(<Clock/>);
-			var seconds = 615;
-			var expected = "10:15"
+			var seconds = 650;
+			var expected = "10:50"
+			var actual = clock.formatSeconds(seconds);
+
+			expect(actual).toBe(expected)
+		})
+
+		it('should format seconds when min/sec are less than 10', () => {
+			var clock = TestUtils.renderIntoDocument(<Clock/>);
+			var seconds = 61;
+			var expected = "01:01"
 			var actual = clock.formatSeconds(seconds);
 
 			expect(actual).toBe(expected)
